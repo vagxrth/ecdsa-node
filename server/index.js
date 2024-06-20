18c9dc18e2087ece543ef453b4215a288fa615b7
Vagarth <vagarth419@gmail.com>
2024-06-21 00:18:03 +0530

    completed week 1 project

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const secp = require('ethereum-cryptography/secp256k1');
+const { keccak256 } = require('ethereum-cryptography/keccak');
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "02c7ec4a57d2b09ccce2f67b9710ec352b507f9b40c6d7f87c6d8a1b6c20f7aac4": 100,
+  "03c4d7908d0bf1704bb1a3c32217b52ae3ae51732839c8a712dcc14e317cecc33e": 50,
+  "0216992de97e8f36802ef40a8cf2303734a2bf160f15e3ab06aa0f8cae810fa277": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,7 +21,20 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, sig:sigStringed, msg } = req.body;
+  const { recipient, amount } = msg;
+
+  const sig = {
+    ...sigStringed,
+    r: BigInt(sigStringed.r),
+    s: BigInt(sigStringed.s)
+  }
+
+  const hashMessage = (message) => keccak256(Uint8Array.from(message));
+
+  const isValid = secp.secp256k1.verify(sig, hashMessage(msg), sender) === true;
+  
+  if(!isValid) res.status(400).send({ message: "Bad signature!"});
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
